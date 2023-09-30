f5a03d897e000c588f2df2b139c680c9ef627a5a
Philipp Freese <philippfreese92@googlemail.com>
2023-09-30 02:08:18 +0200

    Fixes

--- resources/chromium_service_pre_exec.sh
+++ resources/chromium_service_pre_exec.sh
@@ -5,4 +5,3 @@ xset -dpms
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/USER/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/USER/.config/chromium/Default/Preferences
-export URL=CHROMIUM_URL
